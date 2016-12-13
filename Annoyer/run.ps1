abeef2e1deae712d1dc91c6c8ff07b69fdc4d879
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2016-12-13 19:27:00 +0100

    Update run.ps1

--- Annoyer/run.ps1
+++ Annoyer/run.ps1
@@ -1,5 +1,5 @@
 $PayLoad = [PSCustomObject]@{
-    text ="One minute has passed!!! It's currently $((Get-Date))."
+    text ="One minute has passed!!! It's currently $((Get-Date)). From Source Controlled GitHub Repo"
 } | ConvertTo-Json
 
 Invoke-RestMethod -Uri https://hooks.slack.com/services/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 -Method Post -Body $PayLoad
