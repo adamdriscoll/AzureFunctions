f35de3624d8a00894a99331ae76271f022eadc7d
adam.driscoll <adam.driscoll@SBWIENGLP13>
2016-12-13 11:21:50 +0100

    Revert

--- Annoyer/run.ps1
+++ Annoyer/run.ps1
@@ -1,5 +1,5 @@
 $PayLoad = [PSCustomObject]@{
-    text ="One minute has passed!!! It's currently $((Get-Date)). And I was updated via source control!"
+    text ="One minute has passed!!! It's currently $((Get-Date))."
 } | ConvertTo-Json
 
 Invoke-RestMethod -Uri https://hooks.slack.com/services/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 -Method Post -Body $PayLoad
